6d46ffde95b6335514d67454d0bd2ca2dfddae6b
Christopher Boyer <cboyer@poverty-action.org>
2015-08-18 11:09:59 -0400

    first commit

---
+++ R/install.R
@@ -0,0 +1,8 @@
+# required packages for 'Data Visualization with R'
+install.packages("dplyr")
+install.packages("magrittr")
+install.packages("ggplot2")
+install.packages("gridExtra")
+install.packages("ggvis")
+install.packages("rmarkdown")
+install.packages("knitr")
---
+++ readme.md
@@ -0,0 +1,7 @@
+Data Visualization with R
+=========================
+Author: Christopher Boyer
+Date: 27 Aug 2015
+
+## Course Objectives
+The purpose of this course is to
